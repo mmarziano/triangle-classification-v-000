6e56b4e279a98419c07c9f6f454d6a86dc9b632c
Monica Marziano <mmarziano1@gmail.com>
2018-09-17 10:02:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,27 +1,58 @@
-class Triangle
-  attr_accessor :a, :b, :c
-  
-  class TriangleError < StandardError
-    
-  end 
+class Triangle 
+  attr_accessor :a, :b, :c 
   
   def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+  
+  def equilateral?
+    @a == @b && @b == @c 
+  end   
+  
+  def scalene?
+    @a != @b && @b != @c && @a != @c  
+  end 
+  
+  def isosceles?
+    @a == @c && @a != @b || @b == @c && @b != @a || @a == @b && @a != @c 
+  end 
+  
+  def valid?
+    @a > 0 && @b > 0 && @c > 0 
+  end 
+  
+  def legal?
+    @a > 0 && @b > 0 && @c > 0 
+  end 
+  
+  def legal2?
+    @c <= @a + @b || @a <= @b + @c || @b <= @a + @c
   end 
   
   def kind 
-    if @a == 0 && @b == 0 && @c == 0 || @a <= 0 || @b <= 0 || @c <= 0 || @c >= @a + @b ||
-      @a >= @b + @c || @b >= @c + @a 
-      raise TriangleError
-    elsif @a == @b && @b == @c
+    if equilateral? && legal? && legal2?
       return :equilateral 
-    elsif @a == @b || @a == @c || @b == @c && @a != @b
-      return :isosceles
+    elsif 
+      scalene? && legal? && legal2?
+        return :scalene
+    elsif 
+      isosceles? && legal? && legal2?
+      return :isosceles 
     else 
-      return :scalene
+      legal? == false || legal2? == false
+      raise TriangleError
+
     end 
   end
-    
-end
+  
+ 
+  
+  class TriangleError < StandardError 
+     def message 
+       puts "Illegal triangle"
+      end 
+  end   
+
+end 
